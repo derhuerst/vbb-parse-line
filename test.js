64244f20f44dce94ed968ad2304b9fc91da65e02
Jannis R <mail@jannisr.de>
2016-03-30 16:18:19 +0200

    tests for suburban & regional trains

--- test.js
+++ test.js
@@ -7,51 +7,70 @@ const parse  = require('./index.js')
 
 
 
+// todo: https://de.wikipedia.org/wiki/Busverkehr_in_Berlin#Sonderlinien_zur_.E2.80.9ELangen_Nacht_der_Museen.E2.80.9C
+// todo: regional
+// todo: suburban
+// todo: RB40
+// todo: RE1
+// todo: bus "F", "M", "B", …
+
 const tests = {
 
-	// subway
-	  'U7':  {t: 'subway', s: 'U',   nr: 7,    m: false, x: false, n: false}
-	, 'U55': {t: 'subway', s: 'U',   nr: 55,   m: false, x: false, n: false}
+	// subway (ubahn)
+	  'U7':   {t: 'subway',   s: 'U',   nr: 7,    m: false, x: false, n: false}
+	, 'U55':  {t: 'subway',   s: 'U',   nr: 55,   m: false, x: false, n: false}
 
 	// tram
-	, '12':  {t: 'tram',   s: null,  nr: 12,   m: false, x: false, n: false}
-	, '68':  {t: 'tram',   s: null,  nr: 68,   m: false, x: false, n: false}
+	, '12':   {t: 'tram',     s: null,  nr: 12,   m: false, x: false, n: false}
+	, '68':   {t: 'tram',     s: null,  nr: 68,   m: false, x: false, n: false}
 
 	// metro tram
-	, 'M1':  {t: 'tram',   s: 'M',   nr: 1,    m: true,  x: false, n: false}
-	, 'M17': {t: 'tram',   s: 'M',   nr: 17,   m: true,  x: false, n: false}
+	, 'M1':   {t: 'tram',     s: 'M',   nr: 1,    m: true,  x: false, n: false}
+	, 'M17':  {t: 'tram',     s: 'M',   nr: 17,   m: true,  x: false, n: false}
 
 	// bus
-	, '100': {t: 'bus',    s: null,  nr: 100,  m: false, x: false, n: false}
-	, '399': {t: 'bus',    s: null,  nr: 399,  m: false, x: false, n: false}
-	, '893': {t: 'bus',    s: null,  nr: 893,  m: false, x: false, n: false}
+	, '100':  {t: 'bus',      s: null,  nr: 100,  m: false, x: false, n: false}
+	, '399':  {t: 'bus',      s: null,  nr: 399,  m: false, x: false, n: false}
+	, '893':  {t: 'bus',      s: null,  nr: 893,  m: false, x: false, n: false}
 
 	// metro bus
-	, 'M11': {t: 'bus',    s: 'M',   nr: 11,   m: true,  x: false, n: false}
-	, 'M85': {t: 'bus',    s: 'M',   nr: 85,   m: true,  x: false, n: false}
+	, 'M11':  {t: 'bus',      s: 'M',   nr: 11,   m: true,  x: false, n: false}
+	, 'M85':  {t: 'bus',      s: 'M',   nr: 85,   m: true,  x: false, n: false}
 
 	// night bus
-	, 'N1':  {t: 'bus',    s: 'N',   nr: 1,    m: false, x: false, n: true}
-	, 'N97': {t: 'bus',    s: 'N',   nr: 97,   m: false, x: false, n: true}
+	, 'N1':   {t: 'bus',      s: 'N',   nr: 1,    m: false, x: false, n: true}
+	, 'N97':  {t: 'bus',      s: 'N',   nr: 97,   m: false, x: false, n: true}
 
 	// express bus
-	, 'X83': {t: 'bus',    s: 'X',   nr: 83,   m: false, x: true,  n: false}
-	, 'X7':  {t: 'bus',    s: 'X',   nr: 7,    m: false, x: true,  n: false}
-	, 'TXL': {t: 'bus',    s: 'TXL', nr: null, m: false, x: true,  n: false}
+	, 'X83':  {t: 'bus',      s: 'X',   nr: 83,   m: false, x: true,  n: false}
+	, 'X7':   {t: 'bus',      s: 'X',   nr: 7,    m: false, x: true,  n: false}
+	, 'TXL':  {t: 'bus',      s: 'TXL', nr: null, m: true,  x: true,  n: true}
 
 	// ferry
-	, 'F10': {t: 'ferry',  s: 'F',   nr: 10,   m: false, x: false, n: false}
-	, 'F24': {t: 'ferry',  s: 'F',   nr: 24,   m: false, x: false, n: false}
+	, 'F10':  {t: 'ferry',    s: 'F',   nr: 10,   m: false, x: false, n: false}
+	, 'F24':  {t: 'ferry',    s: 'F',   nr: 24,   m: false, x: false, n: false}
+
+	// suburban trains (sbahn)
+	, 'S1':   {t: 'suburban', s: 'S',   nr: 1,    m: false, x: false, n: false}
+	, 'S85':  {t: 'suburban', s: 'S',   nr: 85,   m: false, x: false, n: false}
+
+	// regional trains
+	, 'RE1':  {t: 'regional', s: 'RE',  nr: 1,    m: false, x: true,  n: false}
+	, 'RE78': {t: 'regional', s: 'RE',  nr: 78,   m: false, x: true,  n: false}
+
+	// regional express trains
+	, 'RB10': {t: 'regional', s: 'RB',  nr: 10,   m: false, x: false, n: false}
+	, 'RB93': {t: 'regional', s: 'RB',  nr: 93,   m: false, x: false, n: false}
 
 }
 
 for (let name in tests) {
-	let result = parse(name)
-	let expected = tests[name]
-	assert.strictEqual(parse.type,    expected.t, 'wrong type')
-	assert.strictEqual(parse.symbol,  expected.s, 'wrong symbol')
-	assert.strictEqual(parse.nr,      expected.n, 'wrong numeral')
-	assert.strictEqual(parse.metro,   expected.m, 'wrong metro flag')
-	assert.strictEqual(parse.express, expected.x, 'wrong express flag')
-	assert.strictEqual(parse.night,   expected.n, 'wrong night flag')
+	let r = parse(name)
+	let e = tests[name]
+	assert.strictEqual(r.type,    e.t, `${name} type: ${r.type} !== ${e.t}`)
+	assert.strictEqual(r.symbol,  e.s, `${name} symbol: ${r.symbol} !== ${e.s}`)
+	assert.strictEqual(r.nr,      e.nr, `${name} nr: ${r.nr} !== ${e.nr}`)
+	assert.strictEqual(r.metro,   e.m, `${name} metro: ${r.metro} !== ${e.m}`)
+	assert.strictEqual(r.express, e.x, `${name} express: ${r.express} !== ${e.x}`)
+	assert.strictEqual(r.night,   e.n, `${name} night: ${r.night} !== ${e.n}`)
 }
