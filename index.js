888208a9c943c580663ec7cc9ce449af8d43767d
Jannis R <mail@jannisr.de>
2018-01-17 19:15:39 +0100

    rename type -> product :boom:, add mode

--- index.js
+++ index.js
@@ -7,57 +7,75 @@ const numberOnly = /^[\d]+$/i
 const twoNumbers = /^([\d]+)[^\w]([\d]+)$/i
 const symbolAndNumber = /^(([a-z]{1,3})[^\w]?([\d]+)|([\d]+)[^\w]?([a-z]{1,3}))/i
 
-const types = {
-	  'ICE': 'express'
-	, 'IC': 'express'
-	, 'EC': 'express'
-	, 'EN': 'express'
-	, 'LOC': 'express'
-	, 'RE': 'regional'
-	, 'RB': 'regional'
-	, 'OE': 'regional'
-	, 'U':  'subway'
-	, 'S':  'suburban'
-	, 'F':  'ferry'
-	, 'E':  'bus'
-	, 'H':  'bus'
-	, 'N':  'bus'
-	, 'X':  'bus'
+const modes = {
+	'ICE': 'train',
+	'IC': 'train',
+	'EC': 'train',
+	'EN': 'train',
+	'LOC': 'train',
+	'RE': 'train',
+	'RB': 'train',
+	'OE': 'train',
+	'U':  'train',
+	'S':  'train',
+	'F':  'watercraft',
+	'E':  'bus',
+	'H':  'bus',
+	'N':  'bus',
+	'X':  'bus'
+}
+
+const products = {
+	'ICE': 'express',
+	'IC': 'express',
+	'EC': 'express',
+	'EN': 'express',
+	'LOC': 'express',
+	'RE': 'regional',
+	'RB': 'regional',
+	'OE': 'regional',
+	'U':  'subway',
+	'S':  'suburban',
+	'F':  'ferry',
+	'E':  'bus',
+	'H':  'bus',
+	'N':  'bus',
+	'X':  'bus'
 }
 
 const parse = (name) => {
 	name = name.toUpperCase().replace(leadingProduct, '').replace(spaces, '')
 	let r = {_: name,
-		type: null, symbol: null, nr: null,
+		mode: null, product: null, symbol: null, nr: null,
 		metro: false, express: false, night: false
 	}
 
 	// weird buses in Berlin
 	if (name === 'TXL') return {_: name,
-		type: 'bus', symbol: 'TXL', nr: null,
+		mode: 'bus', product: 'bus', symbol: 'TXL', nr: null,
 		metro: true, express: true, night: true
 	}
 	if (name === 'SXF2') return {_: name,
-		type: 'bus', symbol: 'SXF', nr: 2,
+		mode: 'bus', product: 'bus', symbol: 'SXF', nr: 2,
 		metro: false, express: true, night: false
 	}
 	if (name === 'SEV') return {_: name,
-		type: 'bus', symbol: null, nr: null,
+		mode: 'bus', product: 'bus', symbol: null, nr: null,
 		metro: true, express: false, night: false
 	}
 	if (name === 'IRE') return {_: name,
-		type: 'regional', symbol: 'IRE', nr: null,
+		mode: 'train', product: 'regional', symbol: 'IRE', nr: null,
 		metro: false, express: true, night: false
 	}
 
 	if (symbolOnly.test(name)) {
 		r.symbol = name
-		r.type = 'bus'
+		r.mode = r.product = 'bus'
 	} else if (numberOnly.test(name)) { // bus & tram lines
 		r.nr = parseInt(name)
 	} else if (twoNumbers.test(name)) { // weird buses in Brandenburg
 		const matches = twoNumbers.exec(name)
-		r.type = 'bus'
+		r.mode = r.product = 'bus'
 		r.nr = parseInt(matches[1])
 	} else {
 		const matches = symbolAndNumber.exec(name)
@@ -78,11 +96,17 @@ const parse = (name) => {
 	else if (r.symbol === 'N') r.night   = true
 
 	// handle weird metro bus & metro tram naming
-	if (!r.type) {
-		r.type = types[r.symbol]
-		if (!r.symbol) r.type = r.nr < 100 ? 'tram' : 'bus'
-		else if (r.symbol === 'M')
-			r.type = (r.nr <= 17 && r.nr !== 11) ? 'tram' : 'bus'
+	if (!r.product) {
+		if (r.symbol === 'M') {
+			r.product = (r.nr <= 17 && r.nr !== 11) ? 'tram' : 'bus'
+			r.mode = (r.nr <= 17 && r.nr !== 11) ? 'train' : 'bus'
+		} else if (r.symbol) {
+			r.mode = modes[r.symbol]
+			r.product = products[r.symbol]
+		} else {
+			r.product = r.nr < 100 ? 'tram' : 'bus'
+			r.mode = r.nr < 100 ? 'train' : 'bus'
+		}
 	}
 
 	if (r.symbol === 'RE') r.express = true // regional express trains
